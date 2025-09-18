3e147bad3eaeed4c879b5f383d13958f87278fc8
Justin Zhang <86024349+JusZhaVS@users.noreply.github.com>
2025-09-18 11:01:23 -0400

    adding some more python stuff

--- simulation_of_drawing.py
+++ simulation_of_drawing.py
@@ -13,6 +13,21 @@ def single_trial():
 
         prev_num = new_num
 
+def descending_trial():
+    prev_num = random.random()
+    draws = 1
+
+    while True:
+        next_num = random.random()
+        draws += 1
+        if (next_num > prev_num):
+            return draws
+        prev_num = next_num
+
+def sim_descending(n_trails = 100000):
+    results = [descending_trial() for _ in range(n_trails)]
+    return np.mean(results)
+
 
 def simulate(n_trails = 100000):
     results = [single_trial() for _ in range(n_trails)]
@@ -20,4 +35,7 @@ def simulate(n_trails = 100000):
 
 if __name__ == "__main__":
     estimate = simulate(100000)
+    print(f"Estimated expected number of draws: {estimate:.4f}")
+
+    second_estimate = sim_descending()
     print(f"Estimated expected number of draws: {estimate:.4f}")
