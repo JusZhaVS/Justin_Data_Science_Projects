fb0e497c7e878794dc4227ffd2b5cd2119e77452
Justin Zhang <86024349+JusZhaVS@users.noreply.github.com>
2025-09-19 22:13:15 -0400

    get News feed still bad

--- design_twitter.py
+++ design_twitter.py
@@ -1,7 +1,22 @@
 class Twitter:
 
     def __init__(self):
-        self.followMap = defaultdict(set)
-
+        self.followMap = defaultdict(set) #to store userIds and corresponding followeeIds
+        self.tweetMap = defaultdict(list) = #store userIds and tweets as a min heap of (count, tweetId)
+        self.time = 0
 
     def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweetMap[userId].append([self.time, tweetId])
+        if len(self.tweetMap[userId] > 10):
+            self.tweetMap[userId].pop(0) #pop from the front so popping the oldest element, inefficient but wtv
+        self.time -= 1 #dec cuz min heap, so want newer elements to the front 
+
+#    def getNewsFeed(self, userId: int) -> List[int]:
+        
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followMap[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followMap[followerId]:
+            self.followMap[followerId].remove(followeeId)       
