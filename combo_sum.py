022e6381b2b42ae88143d4928170c7af46b57384
Justin Zhang <86024349+JusZhaVS@users.noreply.github.com>
2025-09-21 11:48:21 -0400

    combo sum again

--- combo_sum.py
+++ combo_sum.py
@@ -6,6 +6,10 @@ def backtrack(start, path total):
     if (total > target):
         return
     
+    for i in range(start, len(nums)):
+        path.append(nums[i])
+        backtrack(i, path, total + nums[i])
+        path.pop()
 
 
 backtrack(0, [], 0)
