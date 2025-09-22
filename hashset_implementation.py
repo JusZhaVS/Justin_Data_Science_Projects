6b94e7c276cf777083b19d197c6eadfd8b9e8d49
Justin Zhang <86024349+JusZhaVS@users.noreply.github.com>
2025-09-21 21:30:53 -0400

    deleted file

---
+++ hashset_implementation.py
@@ -0,0 +1,16 @@
+class MyHashSet:
+
+    def __init__(self):
+        
+    
+    def add(self, key: int) -> None:
+
+    def remove(self, key: int) -> None:
+
+    def contains(self, key: int) -> bool:
+
+
+obj = MyHashSet()
+obj.add(key)
+obj.remove(key)
+param_3 = obj.contains(key)
